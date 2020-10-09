664620274ef8915c5791092a026142fb5f5a1469
shacharharshuv <sharshuv@quotient.com>
2020-10-09 17:10:00 +0300

    Initial Commit

---
+++ Assets/Scripts/PlayerAnimationState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationState : StateMachineBehaviour
+{
+    public float xOffsetOnEnter = 0;
+    public float xOffsetOnExit = 0;
+    
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.transform.position += Vector3.right * xOffsetOnEnter;
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.transform.position += Vector3.right * xOffsetOnExit;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    public float stepLength = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AfterCompletedStep()
+    {
+        Debug.Log("After Completed Step");
+        transform.position += stepLength * Vector3.right;
+    }
+}
